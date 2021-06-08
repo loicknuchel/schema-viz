ca9f2cd5b2e1d93531d69a25eac4488bbd913753
Loïc Knuchel <loic.knuchel@doctolib.com>
2021-06-08 06:45:55 +0200

    Display tables in the middle of the screen

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,12 @@
 module Main exposing (..)
 
 import Browser
+import Browser.Dom
 import Html exposing (Attribute, Html, div, li, text, ul)
 import Html.Attributes exposing (class, style)
 import Http exposing (Error(..))
 import Models.Schema exposing (Column, ColumnName(..), ColumnType(..), Schema, SchemaName(..), Table, TableName(..), schemaDecoder)
+import Task
 
 
 
@@ -23,18 +25,27 @@ colors =
     { red = "#E3342F", pink = "#F66D9B", orange = "#F6993F", yellow = "#FFED4A", green = "#4DC0B5", blue = "#3490DC", darkBlue = "#6574CD", purple = "#9561E2", grey = "#B8C2CC" }
 
 
+type alias Size =
+    { width : Float, height : Float }
+
+
+type alias AbsolutePosition =
+    { top : Int, left : Int }
+
+
 type alias UiTable =
-    { sql : Table, color : String, top : Int, left : Int }
+    { sql : Table, color : String, position : AbsolutePosition }
 
 
-type alias UISchema =
+type alias UiSchema =
     { tables : List UiTable }
 
 
 type Model
     = Loading
-    | Failure Http.Error
-    | Success UISchema
+    | Failure String
+    | Rendering Schema
+    | Success UiSchema
 
 
 init : () -> ( Model, Cmd Msg )
@@ -48,23 +59,37 @@ init _ =
 
 type Msg
     = GotSchema (Result Http.Error Schema)
+    | GotWindowSize Size
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg _ =
+update msg model =
     case msg of
         GotSchema result ->
             case result of
                 Ok schema ->
-                    ( Success (buildUISchema schema), Cmd.none )
+                    ( Rendering schema, windowSize )
 
                 Err e ->
-                    ( Failure e, Cmd.none )
+                    ( Failure (viewHttpError e), Cmd.none )
+
+        GotWindowSize size ->
+            case model of
+                Rendering schema ->
+                    ( Success (buildUiSchema size schema), Cmd.none )
+
+                _ ->
+                    ( Failure "bad", Cmd.none )
 
 
-buildUISchema : Schema -> UISchema
-buildUISchema schema =
-    { tables = List.map (\table -> { sql = table, color = colors.red, top = 0, left = 0 }) schema.tables }
+buildUiSchema : Size -> Schema -> UiSchema
+buildUiSchema size schema =
+    { tables = List.map (\table -> buildUiTable size table) schema.tables }
+
+
+buildUiTable : Size -> Table -> UiTable
+buildUiTable size table =
+    { sql = table, color = colors.red, position = { top = round (size.width / 2), left = round (size.height / 2) } }
 
 
 
@@ -84,10 +109,13 @@ view : Model -> Html Msg
 view model =
     case model of
         Failure e ->
-            text ("Unable to load your schema: " ++ viewHttpError e)
+            text ("Unable to load your schema: " ++ e)
 
         Loading ->
             text "Loading..."
+
+        Rendering _ ->
+            text "Rendering..."
 
         Success structure ->
             div [ class "app" ] (List.map (\table -> viewTable table) structure.tables)
@@ -95,7 +123,7 @@ view model =
 
 viewTable : UiTable -> Html Msg
 viewTable table =
-    div [ class "table", borderColor table.color, top table.top, left table.left ]
+    div [ class "table", borderColor table.color, top table.position.top, left table.position.left ]
         [ div [ class "header" ] [ text (formatTableName table) ]
         , ul [ class "columns" ] (List.map viewColumn table.sql.columns)
         ]
@@ -186,3 +214,8 @@ loadSchema =
         { url = "/test/resources/schema.json"
         , expect = Http.expectJson GotSchema schemaDecoder
         }
+
+
+windowSize : Cmd Msg
+windowSize =
+    Task.perform (\viewport -> GotWindowSize { width = viewport.viewport.width, height = viewport.viewport.height }) Browser.Dom.getViewport

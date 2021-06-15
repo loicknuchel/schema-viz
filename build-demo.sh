f9101e08cf9f548f48aaa905d2b50eb1d3a1afa2
Loïc Knuchel <loic.knuchel@doctolib.com>
2021-06-15 23:26:05 +0200

    Move build demo script to the root folder

--- build-demo.sh
+++ build-demo.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+cd frontend || exit
 cp index.html ../docs
 cp index.css ../docs
 cp tests/resources/schema.json ../docs
 elm make src/Main.elm --optimize --output=../docs/index.js
 sed -i 's/\/tests\/resources\/schema.json/schema.json/' ../docs/index.js
+cd ..

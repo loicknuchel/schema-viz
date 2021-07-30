be03e0e10a54c4260d1198ac95d91f90d9034408
Samir Bouaked <sbouaked@pm.me>
2021-07-30 17:05:47 +0700

    remove unuse import bis

--- src/PagesComponents/Website/View.elm
+++ src/PagesComponents/Website/View.elm
@@ -1,9 +1,7 @@
-module PagesComponents.Website.View exposing (..)
+module PagesComponents.Website.View exposing (viewWebsite)
 
-import FontAwesome.Styles as Icon
-import Html exposing (Html, a, blockquote, button, div, footer, form, h1, h2, h3, header, img, input, label, li, main_, nav, p, span, text, ul)
+import Html exposing (Html, div)
 import Html.Attributes as Attr
-
 import PagesComponents.Website.Views.CtaSection exposing (viewCtaSection)
 import PagesComponents.Website.Views.FeaturesListGridSection exposing (viewFeaturesListGridSection)
 import PagesComponents.Website.Views.FeaturesSection exposing (viewFeaturesSection)
@@ -12,11 +10,10 @@ import PagesComponents.Website.Views.HeaderSection exposing (viewHeaderSection)
 import PagesComponents.Website.Views.HeroSection exposing (viewHeroSection)
 
 
-
 viewWebsite : List (Html msg)
 viewWebsite =
-    [ Icon.css ]
+    []
         ++ [ div
-                [ Attr.class "bg-white"]
+                [ Attr.class "bg-white" ]
                 [ viewHeaderSection, viewHeroSection, viewFeaturesSection, viewFeaturesListGridSection, viewCtaSection, viewFooterSection ]
            ]

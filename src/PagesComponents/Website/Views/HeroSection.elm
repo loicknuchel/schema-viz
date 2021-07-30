61ed741e328ca0bb935d559545609e27483e2874
Samir Bouaked <sbouaked@pm.me>
2021-07-30 16:59:00 +0700

    update components and use Loic wording

--- src/PagesComponents/Website/Views/HeroSection.elm
+++ src/PagesComponents/Website/Views/HeroSection.elm
@@ -1,5 +1,6 @@
 module PagesComponents.Website.Views.HeroSection exposing (viewHeroSection)
 
+import Gen.Route as Route
 import Html exposing (Html, a, blockquote, button, div, footer, form, h1, h2, h3, header, img, input, label, li, main_, nav, p, span, text, ul)
 import Html.Attributes as Attr
 import Svg exposing (path, svg)
@@ -8,4 +9,62 @@ import Svg.Attributes as SvgAttr
 
 viewHeroSection : Html msg
 viewHeroSection =
-    div [] []
+    div
+        [ Attr.class "relative"
+        ]
+        [ div
+            [ Attr.class "absolute inset-x-0 bottom-0 h-1/2 bg-gray-100"
+            ]
+            []
+        , div
+            [ Attr.class "max-w-7xl mx-auto sm:px-6 lg:px-8"
+            ]
+            [ div
+                [ Attr.class "relative shadow-xl sm:rounded-2xl sm:overflow-hidden"
+                ]
+                [ div
+                    [ Attr.class "absolute inset-0"
+                    ]
+                    [ img
+                        [ Attr.class "h-full w-full object-cover"
+                        , Attr.src "https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2830&q=80&sat=-100"
+                        , Attr.alt "People working on laptops"
+                        ]
+                        []
+                    , div
+                        [ Attr.class "absolute inset-0 bg-gradient-to-r from-purple-800 to-indigo-700 mix-blend-multiply"
+                        ]
+                        []
+                    ]
+                , div
+                    [ Attr.class "relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8"
+                    ]
+                    [ h1
+                        [ Attr.class "text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl"
+                        ]
+                        [ span
+                            [ Attr.class "block text-white"
+                            ]
+                            [ text "Schema Viz" ]
+                        ]
+                    , p
+                        [ Attr.class "mt-6 max-w-lg mx-auto text-center text-xl text-indigo-200 sm:max-w-3xl"
+                        ]
+                        [ text "Understand your SQL schema" ]
+                    , div
+                        [ Attr.class "mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center"
+                        ]
+                        [ div
+                            [ Attr.class "space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-1 sm:gap-5"
+                            ]
+                            [ a
+                                [ Attr.href (Route.toHref Route.App)
+                                , Attr.class "flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-indigo-700 bg-white hover:bg-indigo-50 sm:px-8"
+                                ]
+                                [ text "Explore your schema" ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
---
+++ src/PagesComponents/Website/Views/LogoCloudSection.elm
@@ -0,0 +1,73 @@
+module PagesComponents.Website.Views.LogoCloudSection exposing (viewLogoCloudSection)
+
+import Html exposing (Html,  div, img,  p, text)
+import Html.Attributes as Attr
+
+viewLogoCloudSection : Html msg
+viewLogoCloudSection =
+    div
+        [ Attr.class "bg-gray-100"
+        ]
+        [ div
+            [ Attr.class "max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8"
+            ]
+            [ p
+                [ Attr.class "text-center text-sm font-semibold uppercase text-gray-500 tracking-wide"
+                ]
+                [ text "Trusted by over 5 very average small businesses" ]
+            , div
+                [ Attr.class "mt-6 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5"
+                ]
+                [ div
+                    [ Attr.class "col-span-1 flex justify-center md:col-span-2 lg:col-span-1"
+                    ]
+                    [ img
+                        [ Attr.class "h-12"
+                        , Attr.src "https://tailwindui.com/img/logos/tuple-logo-gray-400.svg"
+                        , Attr.alt "Tuple"
+                        ]
+                        []
+                    ]
+                , div
+                    [ Attr.class "col-span-1 flex justify-center md:col-span-2 lg:col-span-1"
+                    ]
+                    [ img
+                        [ Attr.class "h-12"
+                        , Attr.src "https://tailwindui.com/img/logos/mirage-logo-gray-400.svg"
+                        , Attr.alt "Mirage"
+                        ]
+                        []
+                    ]
+                , div
+                    [ Attr.class "col-span-1 flex justify-center md:col-span-2 lg:col-span-1"
+                    ]
+                    [ img
+                        [ Attr.class "h-12"
+                        , Attr.src "https://tailwindui.com/img/logos/statickit-logo-gray-400.svg"
+                        , Attr.alt "StaticKit"
+                        ]
+                        []
+                    ]
+                , div
+                    [ Attr.class "col-span-1 flex justify-center md:col-span-2 md:col-start-2 lg:col-span-1"
+                    ]
+                    [ img
+                        [ Attr.class "h-12"
+                        , Attr.src "https://tailwindui.com/img/logos/transistor-logo-gray-400.svg"
+                        , Attr.alt "Transistor"
+                        ]
+                        []
+                    ]
+                , div
+                    [ Attr.class "col-span-2 flex justify-center md:col-span-2 md:col-start-4 lg:col-span-1"
+                    ]
+                    [ img
+                        [ Attr.class "h-12"
+                        , Attr.src "https://tailwindui.com/img/logos/workcation-logo-gray-400.svg"
+                        , Attr.alt "Workcation"
+                        ]
+                        []
+                    ]
+                ]
+            ]
+        ]

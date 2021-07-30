e7d015f12e1eb3064f958f1ad439b8d35489dc1a
Samir Bouaked <sbouaked@pm.me>
2021-07-30 17:05:47 +0700

    remove unuse import bis

--- src/PagesComponents/Website/Views/HeroSection.elm
+++ src/PagesComponents/Website/Views/HeroSection.elm
@@ -1,10 +1,8 @@
 module PagesComponents.Website.Views.HeroSection exposing (viewHeroSection)
 
 import Gen.Route as Route
-import Html exposing (Html, a, blockquote, button, div, footer, form, h1, h2, h3, header, img, input, label, li, main_, nav, p, span, text, ul)
+import Html exposing (Html, a, div, h1, img, p, span, text)
 import Html.Attributes as Attr
-import Svg exposing (path, svg)
-import Svg.Attributes as SvgAttr
 
 
 viewHeroSection : Html msg

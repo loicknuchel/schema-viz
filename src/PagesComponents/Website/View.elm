d9678bc57994e22577d32fde2b7ec1965daab27a
Samir Bouaked <sbouaked@pm.me>
2021-07-30 15:56:08 +0700

    separate in diff sections

--- src/PagesComponents/Website/View.elm
+++ src/PagesComponents/Website/View.elm
@@ -1,14 +1,22 @@
 module PagesComponents.Website.View exposing (..)
 
-import Css.Global
 import FontAwesome.Styles as Icon
-import Html exposing (Html)
-import Html.Attributes exposing (..)
-import Html.Styled as SHtml
-import PagesComponents.Website.Views.HeaderSection exposing (viewHeader)
+import Html exposing (Html, a, blockquote, button, div, footer, form, h1, h2, h3, header, img, input, label, li, main_, nav, p, span, text, ul)
+import Html.Attributes as Attr
+
+import PagesComponents.Website.Views.CtaSection exposing (viewCtaSection)
+import PagesComponents.Website.Views.FeaturesListGridSection exposing (viewFeaturesListGridSection)
+import PagesComponents.Website.Views.FeaturesSection exposing (viewFeaturesSection)
+import PagesComponents.Website.Views.FooterSection exposing (viewFooterSection)
+import PagesComponents.Website.Views.HeaderSection exposing (viewHeaderSection)
+import PagesComponents.Website.Views.HeroSection exposing (viewHeroSection)
+
 
 
 viewWebsite : List (Html msg)
 viewWebsite =
     [ Icon.css ]
-        ++ [ viewHeader ]
+        ++ [ div
+                [ Attr.class "bg-white"]
+                [ viewHeaderSection, viewHeroSection, viewFeaturesSection, viewFeaturesListGridSection, viewCtaSection, viewFooterSection ]
+           ]
---
+++ src/PagesComponents/Website/Views/HeroSection.elm
@@ -0,0 +1,11 @@
+module PagesComponents.Website.Views.HeroSection exposing (viewHeroSection)
+
+import Html exposing (Html, a, blockquote, button, div, footer, form, h1, h2, h3, header, img, input, label, li, main_, nav, p, span, text, ul)
+import Html.Attributes as Attr
+import Svg exposing (path, svg)
+import Svg.Attributes as SvgAttr
+
+
+viewHeroSection : Html msg
+viewHeroSection =
+    div [] []

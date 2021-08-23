1709ff59e077c0e1798e37790381d57910cd298c
Samir Bouaked <sbouaked@pm.me>
2021-08-23 13:58:19 +0200

    full screen elm book

--- src/Components/Book.elm
+++ src/Components/Book.elm
@@ -6,11 +6,11 @@ import Components.Organisms.Header exposing (headerChapter)
 import Components.Slices.Feature exposing (featureChapter)
 import Components.Slices.Hero exposing (heroChapter)
 import Css.Global exposing (global)
-import ElmBook exposing (withChapterGroups, withThemeOptions)
+import ElmBook exposing (withChapterGroups, withComponentOptions, withThemeOptions)
 import ElmBook.ElmCSS exposing (Book, book)
 import ElmBook.ThemeOptions
+import ElmBook.ComponentOptions
 import Tailwind.Utilities exposing (globalStyles)
-
 
 main : Book x
 main =
@@ -18,6 +18,9 @@ main =
         |> withThemeOptions
             [ ElmBook.ThemeOptions.subtitle "v1.0.1"
             , ElmBook.ThemeOptions.globals [ global globalStyles ]
+            ]
+        |> withComponentOptions
+            [ ElmBook.ComponentOptions.fullWidth True
             ]
         |> withChapterGroups
             [ ( "Atoms"

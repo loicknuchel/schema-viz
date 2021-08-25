167173660d87a5d7a2c65f3b45b2b2086fc9015b
Loïc Knuchel <loic.knuchel@doctolib.com>
2021-08-25 09:35:44 +0200

    change service worker strategy

--- public/service-worker.js
+++ public/service-worker.js
@@ -32,9 +32,7 @@ const assets = [
 
 self.addEventListener('install', installEvent => {
     // console.log(`Installing V${version}...`, installEvent)
-    installEvent.waitUntil(
-        caches.open(assetsCache).then(cache => cache.addAll(assets))
-    )
+    installEvent.waitUntil(cacheAll(assets))
 })
 
 self.addEventListener('activate', activateEvent => {
@@ -45,15 +43,39 @@ self.addEventListener('fetch', fetchEvent => {
     // console.log(`Fetch in V${version}.`, fetchEvent)
     const request = fetchEvent.request
     if (request.method === 'GET') {
-        fetchEvent.respondWith(
-            caches.open(assetsCache).then(cache =>
-                fetch(request)
-                    .then(response => {
-                        cache.put(request, response.clone())
-                        return response
-                    })
-                    .catch(err => cache.match(request).then(response => response || Promise.reject(err)))
-            )
-        )
+        fetchEvent.respondWith(getFromCache(request).catch(err => {
+            console.warn(err)
+            return fetch(request)
+        }))
+        fetchEvent.waitUntil(updateCache(request))
     }
 })
+
+function cacheAll(urls) {
+    return caches.open(assetsCache).then(cache => cache.addAll(urls))
+}
+
+function getFromCache(request) {
+    return caches.open(assetsCache)
+        .then(cache => cache.match(request))
+        .then(response => response || Promise.reject(new Error(`service worker cache miss for ${request.method} ${request.url}`)))
+}
+
+function updateCache(request) {
+    return fetch(request).then(response =>
+        caches.open(assetsCache).then(cache =>
+            cache.put(request, response)
+        )
+    )
+}
+
+function fetchAndUpdateCache(request) {
+    return caches.open(assetsCache).then(cache =>
+        fetch(request)
+            .then(response => {
+                cache.put(request, response.clone())
+                return response
+            })
+            .catch(err => cache.match(request).then(response => response || Promise.reject(err)))
+    )
+}
